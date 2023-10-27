9f5ca8f902ae326a6a6151a7e3484a33b12cb42e
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2023-10-27 06:39:23 -0300

    código inicial

---
+++ src/main/java/ifpr/pgua/eic/base/App.java
@@ -0,0 +1,13 @@
+package ifpr.pgua.eic.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
